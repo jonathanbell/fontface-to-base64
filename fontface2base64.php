79345cc1abf44cfc6de1961ed17587005f8bedcc
Jonathan Bell <jonathanbell.ca@gmail.com>
2015-10-24 21:37:54 -0700

    swtich to php

---
+++ fontface2base64.php
@@ -0,0 +1,29 @@
+<?php
+
+// ***USAGE (command line):
+// php fontface2base64.php </path/to/font/file.[otf|ttf]> <font-name> <font-style[italic|normal]> <font-weight[300|400|700]>
+// OTF or TTF? http://superuser.com/questions/96390/difference-between-otf-open-type-or-ttf-true-type-font-formats
+// There's no validation because you know what you're doing..
+
+$fontFile = $argv[1]; // $argv[0] is the call to the script itself
+$fontName = $argv[2];
+$fontStyle = $argv[3];
+$fontWeight = $argv[4];
+
+if (substr($fontFile, -3) == 'otf') {
+  $fontType = 'opentype';
+} elseif (substr($fontFile, -3) == 'ttf') {
+  $fontType = 'truetype';
+} else {
+  echo 'No OTF or TTF font file supplied. Exiting...';
+  exit();
+}
+
+if (!file_exists($fontFile)) {
+  echo 'The file '.$fontFile.' does not exist.';
+  exit();
+}
+
+$str = '@font-face {'.PHP_EOL.'  font-family:\''.$fontName.'\';font-style:'.$fontStyle.';font-weight:'.$fontWeight.';src:url(data:font/'.$fontType.';base64,'.base64_encode(file_get_contents($fontFile)).')'.PHP_EOL.'}'.PHP_EOL;
+
+file_put_contents('./'.$fontName.'.css', $str); 
